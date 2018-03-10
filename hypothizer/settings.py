bee7967fe9be778969e423d67f6d546b68065299
Abhinav Singh <sudo.abhinav@gmail.com>
2018-03-10 23:17:36 +0530

    Remove static files and related config

--- hypothizer/settings.py
+++ hypothizer/settings.py
@@ -113,15 +113,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 
 CSV_FILE_NAME = 'DelhiPopulationData.csv'
 CSV_FILE_PATH = os.path.join(BASE_DIR, CSV_FILE_NAME)
