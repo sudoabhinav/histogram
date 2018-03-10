b3c4ea3ff42ee28cc0752d6a1222958f3855eb83
Abhinav Singh <sudo.abhinav@gmail.com>
2018-03-10 21:26:01 +0530

    Add home page view
     - Add home page view
     - Add home page url pattern
     - Add template for home page; change settings to use different templates dir

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    """docstring for HomeView"""
+    template_name = "app/home.html"
